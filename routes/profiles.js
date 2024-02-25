a15b03efb8be4dbb523bd7b548a2397d56723c06
max <existentiary@gmail.com>
2024-02-25 22:25:54 +0300

    250224

--- routes/profiles.js
+++ routes/profiles.js
@@ -3,38 +3,64 @@ var router = express.Router();
 const mongoClient = require('../mongo');
 
 
-router.get('/profiles', async function(req, res, next) {
-  const profiles = await mongoClient.getProfiles(); 
+router.get('/profiles', async function(req, res, next){
+  let profiles = [];
+  try {
+    profiles = await mongoClient.getProfiles();
+  } catch(err){
+    console.log(err);
+  }
+
   res.send(profiles);
 });
 
-router.get('/profile/:id', async function(req, res, next) {
-  const profile = await mongoClient.getProfile(req.params.id); 
+router.get('/profile/:id', async function(req, res, next){
+  let profile = {
+    id: '',
+    name: '',
+    frames: {},
+  }
+  try {
+    profile = await mongoClient.getProfile(req.params.id);
+  } catch(err){
+    console.log(err);
+  }
+
   res.send(profile);
 });
 
-router.post('/profile', async function(req, res, next) {
+router.post('/profile', async function(req, res, next){
   const data = req.body;
   const profileId = data.id;
   let profile = null;
+  let respData = {id: null}
 
-  if(profileId){
-    profile = await mongoClient.update(data);
-  } else {
-    profile = await mongoClient.create(data);
+  try {
+    if(profileId){
+      profile = await mongoClient.update(data);
+    } else {
+      profile = await mongoClient.create(data);
+    }
+    respData.id = profile.insertedId ?? data.id;
+  } catch(err){
+    console.log(err);
   }
 
-  res.send({
-    id: profile.insertedId ?? data.id
-  });
+  res.send(respData);
 });
 
-router.delete('/profile/:id', async function(req, res, next) {
+router.delete('/profile/:id', async function(req, res, next){
   const profileId = req.params.id;
-  const result = await mongoClient.delete(profileId); 
-  res.send({
-    id: result.deletedCount > 0 ? profileId : '',
-  });
+  let respData = {id: ''}
+
+  try {
+    const result = await mongoClient.delete(profileId);
+    respData.id = result.deletedCount > 0 ? profileId : '';
+  } catch(err){
+    console.log(err);
+  }
+
+  res.send(respData);
 });
 
 module.exports = router;
